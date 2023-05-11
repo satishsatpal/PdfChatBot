2ef913757a51f1e70a53d5285a364a2e519c9629
satishsatpal <93033998+satishsatpal@users.noreply.github.com>
2023-05-11 10:08:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,166 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+from datetime import datetime
+import time
+
+import mysql.connector
+from mysql.connector import Error
+
+# Setting page title and header
+st.set_page_config(page_title="AgreedBot", page_icon=":robot_face:")
+st.markdown("<h1 style='text-align: center;'>Agronomist - Information assistant</h1>", unsafe_allow_html=True)
+
+# Set org ID and API key
+#openai.organization = "<YOUR_OPENAI_ORG_ID>"
+openai.api_key = "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"
+
+# Initialise session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+if 'model_name' not in st.session_state:
+    st.session_state['model_name'] = []
+if 'cost' not in st.session_state:
+    st.session_state['cost'] = []
+if 'total_tokens' not in st.session_state:
+    st.session_state['total_tokens'] = []
+if 'total_cost' not in st.session_state:
+    st.session_state['total_cost'] = 0.0
+
+# Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
+st.sidebar.title("Sidebar")
+model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+counter_placeholder = st.sidebar.empty()
+counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
+
+# Map model names to AI model IDs
+if model_name == "GPT-3.5":
+    model = "gpt-3.5-turbo"
+else:
+    model = "gpt-4"
+
+# reset everything
+if clear_button:
+    st.session_state['generated'] = []
+    st.session_state['past'] = []
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+    st.session_state['number_tokens'] = []
+    st.session_state['model_name'] = []
+    st.session_state['cost'] = []
+    st.session_state['total_cost'] = 0.0
+    st.session_state['total_tokens'] = []
+    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+
+def sidebar_bg(side_bg):
+
+   side_bg_ext = 'png'
+
+   st.markdown(
+      f"""
+      <style>
+      [data-testid="stSidebar"] > div:first-child {{
+          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+      }}
+      </style>
+      """,
+      unsafe_allow_html=True,
+      )
+#st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
+# generate a response
+def generate_response(prompt):
+    st.session_state['messages'].append({"role": "user", "content": prompt})
+
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=st.session_state['messages']
+    )
+    response = completion.choices[0].message.content
+    st.session_state['messages'].append({"role": "assistant", "content": response})
+
+    # print(st.session_state['messages'])
+    total_tokens = completion.usage.total_tokens
+    prompt_tokens = completion.usage.prompt_tokens
+    completion_tokens = completion.usage.completion_tokens
+    return response, total_tokens, prompt_tokens, completion_tokens
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+        st.session_state['model_name'].append(model_name)
+        st.session_state['total_tokens'].append(total_tokens)
+
+        # from https://openai.com/pricing#language-models
+        if model_name == "GPT-3.5":
+            cost = total_tokens * 0.002 / 1000
+        else:
+            cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+
+        st.session_state['cost'].append(cost)
+        st.session_state['total_cost'] += cost
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
+
+
+            try:
+                connection = mysql.connector.connect(host='192.168.2.13',
+                                  database='kidneyapp',
+                                  user='Kidney',
+                                  password='Kidney@123',use_pure=True)
+
+                if connection.is_connected():
+
+                    db_Info = connection.get_server_info()
+                    print("Connected to MySQL Server version ", db_Info)
+                    cursor = connection.cursor()
+
+                    upload_date_query = "select DATE_FORMAT(NOW(), '%m-%d-%Y %T') from dual"
+                    cursor.execute(upload_date_query)
+                    upload_date_new22 = cursor.fetchall()
+                    new_dd = str(upload_date_new22[0][0])  
+                  
+                    connection.commit()          
+                    insert_query = "insert into chat_logs(date,query,response) values(%s,%s,%s)"
+                    update_values = (new_dd,st.session_state['past'][i],st.session_state["generated"][i])
+                    cursor.execute(insert_query,update_values)
+                    connection.commit()
+
+            except Error as e:
+                print("Error while connecting to MySQL", e)
+
+            finally:
+                if connection.is_connected():
+                    cursor.close()
+                    connection.close() 
+
+            st.write(
+                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
+            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
